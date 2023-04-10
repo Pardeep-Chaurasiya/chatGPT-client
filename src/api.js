2bba1e74dd81757c89dabf79045c36b77f445495
Pardeep Chaurasiya <chaurasiyapardeep001@gmail.com>
2023-04-10 06:10:45 +0530

    url updated

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export const fetchResponse = async(chat) =>{
     try {
-        const response = await fetch('http://localhost:3000/',{
+        const response = await fetch('https://chat-gpt-server-omega.vercel.app/',{
             method:"POST",
             headers:{
                 "Content-Type":"application/json"
